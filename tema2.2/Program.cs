e639eceaa583ac1c01cc67c0a3cfc482bb90c5c6
yukarrr <yukarrr8@gmail.com>
2024-11-19 14:23:01 +0900

    Initial commit

---
+++ tema2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema2._2
+
+{
+    public class Person
+    {
+        public string Fullname { get; set; }
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string email { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person user = new Person
+            {
+                Fullname = "Карамова",
+                Name = "Юлия",
+                Lastname = "Максимовна",
+                email = "yulyakaramova@yandex.ru",
+            };
+
+            Console.WriteLine("Имя пользователя: " + " " + user.Fullname + " " + user.Name + " " + user.Lastname);
+            Console.WriteLine($"Электронная почта: {user.email}");
+            Console.ReadLine();
+        }
+    }
+}
